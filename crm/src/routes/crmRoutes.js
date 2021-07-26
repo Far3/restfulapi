7e82022ef49df98067f25061f2065db7286089d2
far3 <franklynroth@gmail.com>
2021-07-25 22:21:56 -0600

    Update put endpoint

--- crm/src/routes/crmRoutes.js
+++ crm/src/routes/crmRoutes.js
@@ -1,5 +1,9 @@
 import { get } from 'mongoose';
-import { addNewContact, getContacts, getContactWithId } from '../controllers/crmController';
+import { addNewContact,
+        getContacts,
+        getContactWithId,
+        updateContact
+        } from '../controllers/crmController';
 
 const routes = (app) => {
     app.route('/contact')
@@ -14,8 +18,7 @@ const routes = (app) => {
 
     app.route('/contact/:contactId')
         .get(getContactWithId)
-        .put((req, res) =>
-            res.send('PUT request successful!'))
+        .put(updateContact)
 
         .delete((req, res) =>
             res.send('DELETE request successful!'))
